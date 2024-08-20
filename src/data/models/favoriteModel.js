2825c5ff5f4e7cbd575f760d0979ab92582f4e3f
KevinEjiofor <ejioforkelvin@gmail.com>
2024-08-20 07:22:46 +0100

    finish debug and correcting some end point

--- src/data/models/favoriteModel.js
+++ src/data/models/favoriteModel.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
 const favoriteSchema = new mongoose.Schema({
-  recipeId: { type: String, required: true },
-  title: { type: String, required: true },
-  ingredients: [String],
+    recipeId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    ingredients: {
+        type: [String],
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true, 
+        default: 'default-image-url.jpg',
+    }
+}, {
+    timestamps: true,
 });
 
 const Favorite = mongoose.model('Favorite', favoriteSchema);
--- src/services/favoriteService.js
+++ src/services/favoriteService.js
@@ -1,17 +1,29 @@
 const Favorite = require('../data/models/favoriteModel');
 
 const saveFavorite = async (recipeData) => {
-  const favorite = new Favorite(recipeData);
-  await favorite.save();
-  return favorite;
+  const existingFavorite = await doesFavoriteExist(recipeData.recipeId);
+    
+  if (existingFavorite) {
+      return existingFavorite;  
+      }
+
+  
+    const favorite = new Favorite(recipeData);
+    await favorite.save();
+    return favorite;
 };
 
 const getFavorites = async () => {
-  return await Favorite.find();
+    return await Favorite.find();
 };
 
 const deleteFavorite = async (id) => {
-  await Favorite.findByIdAndDelete(id);
+    await Favorite.findByIdAndDelete(id);
 };
+
+const doesFavoriteExist = async (recipeId) => {
+  return await Favorite.findOne({ recipeId });
+};
+
 
 module.exports = { saveFavorite, getFavorites, deleteFavorite };
